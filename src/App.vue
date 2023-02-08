b540e6c56dd90e7899de20dd7ce2b5919ce63979
Nurullakh <nurullakh.akhmedov@mail.ru>
2023-02-08 08:44:26 +0300

    Убрал лищнее

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <h1>hello</h1>
-    <h2>hello</h2>
-    <h3>hello</h3>
-    <h4>hello</h4>
   </div>
 </template>
 
