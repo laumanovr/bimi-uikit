6f688d35cf2c1ea80261c811d9001df230b0ef6c
Nurullakh <nurullakh.akhmedov@mail.ru>
2023-02-08 08:43:07 +0300

    Сделал величину в rem и перенес стили в папку fonts

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>hello</h1>
+    <h2>hello</h2>
+    <h3>hello</h3>
+    <h4>hello</h4>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
