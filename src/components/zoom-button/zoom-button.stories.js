5bdca60b17ab8fb015f33b2e90759b4919cd3103
Vitaliy Novoseltsev <vitalik.novoselcev95@gmail.com>
2023-02-24 11:25:43 +0300

    fix storybook; add storybook build

--- src/components/zoom-button/zoom-button.stories.js
+++ src/components/zoom-button/zoom-button.stories.js
@@ -1,12 +1,22 @@
-import VButton from "./zoom-button";
+import ZoomButton from "./zoom-button";
 
 export default {
   title: "zoom-button",
-  component: VButton,
+  component: ZoomButton,
 };
 
-export const Primary = (args, { argTypes }) => ({
-  components: { VButton },
+const Template = (args, { argTypes }) => ({
+  components: { ZoomButton },
   props: Object.keys(argTypes),
-  template: '<zoom-button v-bind="$props" v-on="$props" />',
+  template: `<zoom-button v-bind="$props" v-on="$props">Zoom</zoom-button>`,
 });
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  type: 'width',
+  disabled: false,
+  plus: () => { console.log('plus'); },
+  minus: () => { console.log('minus'); },
+  default: ''
+};
