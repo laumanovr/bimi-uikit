cda1978df55e4cd019553a091c691b40a04ff02e
Ramazan <laumanov.r@gmail.com>
2023-03-16 14:56:02 +0600

    create divider-line component

---
+++ src/components/v-divider/v-divider.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="divider-line" :class="{'divider-line__bold': isBold}"></div>
+</template>
+
+<script>
+export default {
+  name: 'v-divider',
+  props: {
+    isBold: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../../styles/_colors";
+.divider-line {
+  height: 1px;
+  margin: 5px 0;
+  background: $grey-300;
+  &__bold {
+    background: $grey-650;
+  }
+}
+</style>
