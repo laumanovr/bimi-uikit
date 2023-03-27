774b1ce48920d930193376be19544a0e69901f85
Ramazan <ramazan@bimiboo.com>
2023-03-27 17:59:52 +0600

    modified input-search-stories

--- src/components/input-form/input-form.stories.js
+++ src/components/input-form/input-form.stories.js
@@ -10,22 +10,26 @@ export const FormInput = (args, {argTypes}) => ({
     props: Object.keys(argTypes),
     data() {
         return {
-            inputValue: '',
-            inputLinearValue: 'linear'
+            inputValue1: '',
+            inputValue2: '',
+            inputValue3: '',
+            inputLinearValue1: '',
+            inputLinearValue2: '',
+            inputLinearValue3: ''
         }
     },
     template: `
       <div style="margin-top: 20px; display: flex">
           <div style="margin-right: 80px; min-width: 310px">
-            <input-form v-model="inputValue" style="margin-bottom: 15px;"/>
-            <input-form is-correct style="margin-bottom: 15px;"/>
-            <input-form error-msg="Error message" style="margin-bottom: 15px;"/>
+            <input-form v-model="inputValue1" style="margin-bottom: 15px;"/>
+            <input-form v-model="inputValue2" is-correct style="margin-bottom: 15px;"/>
+            <input-form v-model="inputValue3" error-msg="Error message" style="margin-bottom: 15px;"/>
             <input-form is-disabled label-name="Disabled" style="margin-bottom: 15px;"/>
           </div>
           <div style="min-width: 310px">
-            <input-form v-model="inputLinearValue" is-linear style="margin: 20px 0 25px;"/>
-            <input-form is-linear is-correct style="margin-bottom: 25px;"/>
-            <input-form is-linear error-msg="Error message" style="margin-bottom: 25px;"/>
+            <input-form v-model="inputLinearValue1" is-linear style="margin: 20px 0 25px;"/>
+            <input-form v-model="inputLinearValue2" is-linear is-correct style="margin-bottom: 25px;"/>
+            <input-form v-model="inputLinearValue3" is-linear error-msg="Error message" style="margin-bottom: 25px;"/>
             <input-form is-linear is-disabled label-name="Disabled" style="margin-bottom: 15px;"/>
           </div>
       </div>`
