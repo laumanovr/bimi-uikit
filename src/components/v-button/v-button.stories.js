dbc9ffd805fecb786ebb2c0a2f7e1edae2ecea89
Nurullakh <nurullakh.akhmedov@mail.ru>
2023-03-29 16:43:07 +0300

    Добавил все состояние кнопок в документацию

--- src/components/v-button/v-button.stories.js
+++ src/components/v-button/v-button.stories.js
@@ -9,6 +9,29 @@ export const Button = (args, { argTypes }) => ({
   components: { VButton },
   props: Object.keys(argTypes),
   template: `<div>
-      <v-button v-bind="$props" v-on="$props" />
+      <v-button icon="ic-apple" v-bind="$props" v-on="$props" />
+    </div>`,
+});
+
+export const Secondary = (args, { argTypes }) => ({
+  components: { VButton },
+  props: Object.keys(argTypes),
+  template: `<div>
+      <v-button theme="secondary" v-bind="$props" v-on="$props" />
+    </div>`,
+});
+
+export const Tertiary = (args, { argTypes }) => ({
+  components: { VButton },
+  props: Object.keys(argTypes),
+  template: `<div>
+      <v-button theme="tertiary" v-bind="$props" v-on="$props" />
+    </div>`,
+});
+export const Floating = (args, { argTypes }) => ({
+  components: { VButton },
+  props: Object.keys(argTypes),
+  template: `<div>
+      <v-button theme="floating" v-bind="$props" v-on="$props" />
     </div>`,
 });
